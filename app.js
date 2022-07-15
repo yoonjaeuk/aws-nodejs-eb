7e530b0cec2d8c9bf697834436a16cfe84c3d3e7
S3CloudHub <108780578+S3CloudHub@users.noreply.github.com>
2022-07-15 21:56:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'BackSpace Academy & Elastic Beanstalk'
+    title: 'S3 CloudHub & Elastic Beanstalk'
     })
     res.status(200).end();
 })
